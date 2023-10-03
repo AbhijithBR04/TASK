e2936223b607bd973ab6a476115277940a52c1fb
AbhijithBR04 <abhijith.br@chordify.com>
2023-10-03 10:06:33 +0530

    first commit

--- javascript/javsc.js
+++ javascript/javsc.js
@@ -1,5 +1,11 @@
 // document.write("abhi")
 
+// let a="abhijith bhadran r "
+// let b=a.slice(-9)
+// console.log(b)
+
+
+
 // let a=[1,2,3,4,5]
 
 // for (i in a){
@@ -7,15 +13,25 @@
 //     console.log(i)
 // }
 
+//*Star pattern*//
+// let rows = 6;
+// let char = "";
 
-let rows = 6;
-let char = "";
+// for (let i = 0; i < rows; i++) { 
+//   for (let k = 0; k < rows - i; k++) {
+//     char += "*";
+//   }
+//   char += "\n";
+// }
+// console.log(char);
 
-for (let i = 0; i < rows; i++) {
-  // printing star
-  for (let k = 0; k < rows - i; k++) {
-    char += "*";
-  }
-  char += "\n";
-}
-console.log(char);
+
+
+//*Events*
+// function up(){
+//     document.getElementById("red").style.color="red";
+// }
+
+
+
+ 
