d644f1083481b6fe4b967c18c4188d6ffaaf6f59
AbhijithBR04 <146345263+AbhijithBR04@users.noreply.github.com>
2023-10-27 14:59:30 +0530

    basics redux

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
-import React,{ useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import logo from './logo.svg';
+import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import { Home } from './components/Home'
+import { About } from './components/About'
+import { Navbar } from './components/Navbar';
+import Joke from './components/Joke';
 
-import { AddTodoAction, RemoveTodoAction } from "./actions/TodoActions";
-import "./App.css";
 
 function App() {
-  const [todo, setTodo] = useState();
-
-  const dispatch = useDispatch();
-  const Todo = useSelector((state) => state.Todo);
-  const { todos } = Todo;
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(AddTodoAction(todo));
-  };
-
-  const removeHandler = (t) => {
-    console.log(t);
-    dispatch(RemoveTodoAction(t));
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Enter a Task</h2>
-        <form onSubmit={handleSubmit}>
-          <input
-            placeholder="Enter a task"
-            style={{
-              width: 200,
-              padding: 20,
-              borderRadius: 20,
-              border: "none",
-              fontSize: 20,
-            }}
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <button
-            type="submit"
-            style={{
-              padding: 20,
-              borderRadius: 25,
-              fontSize: 20,
-              marginLeft: 20,
-            }}
-          >
-            ADD
-          </button>
-          <ul>
-            {todos &&
-              todos.map((t) => (
-                <li key={t}>
-                  <span>{t}</span>
-                  <button
-                    type="button"
-                    style={{
-                      borderRadius: 25,
-                      fontSize: 20,
-                      marginLeft: 20,
-                      color: "white",
-                      backgroundColor: "blue",
-                    }}
-                    onClick={() => removeHandler(t)}
-                  >
-                    delete
-                  </button>
-                </li>
-              ))}
-          </ul>
-        </form>
-      </header>
-    </div>
+    <>
+      <Navbar/>
+        <Routes>                  
+             <Route path='/' element={ <Home /> }></Route>
+             <Route path='About' element={<About />}></Route>
+             <Route path='Joke' element={<Joke />}></Route>
+        </Routes>
+    </>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
+import Context from './components/Context';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+      <Context/>
+    </BrowserRouter>  
+    
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
